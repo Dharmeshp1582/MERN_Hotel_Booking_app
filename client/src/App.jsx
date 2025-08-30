6c9d317552b5cadb7125e4ac366e631e66799e22
Dharmeshp1582 <alpeshpatelvirpur@gmail.com>
2025-08-30 11:11:59 +0530

    room detail page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home';
-import Footer from './components/Footer';
-import AllRooms from './pages/AllRooms';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
+import RoomDetail from "./pages/RoomDetail";
 
 const App = () => {
-
- const isOwnerPath = useLocation().pathname.includes("owner");
-
+  const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <div>
       {!isOwnerPath && <Navbar />}
-      <div className='min-h-[70vh]'>
-   <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/rooms' element={<AllRooms />} />
-   </Routes>
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetail />} />
+        </Routes>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
